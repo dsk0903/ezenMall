ab635e23f05a5464ef550a209ef5b8c56b5c6c8d
82107 <82107@JiHoon>
2022-10-31 22:59:51 +0900

    2022-10-31 admin <jihoon>

--- EzenMall/src/main/java/ezen/mall/spring/admin/dao/AdminDAO.java
+++ EzenMall/src/main/java/ezen/mall/spring/admin/dao/AdminDAO.java
@@ -1,5 +1,18 @@
 package ezen.mall.spring.admin.dao;
 
-public class AdminDAO {
+import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ezen.mall.spring.admin.dto.AdminDTO;;
+
+@Repository
+public class AdminDAO {
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
+	
+	public List<AdminDTO> selectAll(){
+		return sqlSessionTemplate.selectList("selectAll");
+	}
 }
--- EzenMall/src/main/java/ezen/mall/spring/admin/dto/AdminDTO.java
+++ EzenMall/src/main/java/ezen/mall/spring/admin/dto/AdminDTO.java
@@ -1,5 +1,16 @@
 package ezen.mall.spring.admin.dto;
 
-public class AdminDTO {
+import org.springframework.stereotype.Component;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Component
+@Setter
+@Getter
+public class AdminDTO {
+	private String member_id;
+	private String member_pwd;
+	private String member_name;
 }
