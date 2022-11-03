3af39a21acd9bad3d6eb66fa6d516ce86ec51cda
82107 <82107@JiHoon>
2022-11-04 00:32:46 +0900

    2022-11-03 지훈 - 회원 관리

--- EzenMall/src/main/java/ezen/mall/spring/admin/dto/AdminDTO.java
+++ EzenMall/src/main/java/ezen/mall/spring/admin/dto/AdminDTO.java
@@ -14,6 +14,14 @@ public class AdminDTO {
 	private String member_id;
 	private String member_pwd;
 	private String member_name;
+	private String member_email;
+	private String member_nickname;
+	private String member_profile;
+	private String member_check;
+	private int member_role;
+	private String member_grade;
+	private String member_phone;
+	private String member_date;
 	
 	private int product_number;
 	private String category_code;
